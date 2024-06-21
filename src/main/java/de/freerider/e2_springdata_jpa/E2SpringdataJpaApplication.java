c4798cc8fe92c207dad79e58a69cf06d44bbc28b
Timon Trettin <timon.trettin@icloud.com>
2024-06-21 18:21:58 +0200

    add stuff

---
+++ src/main/java/de/freerider/e2_springdata_jpa/E2SpringdataJpaApplication.java
@@ -0,0 +1,13 @@
+package de.freerider.e2_springdata_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E2SpringdataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E2SpringdataJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/freerider/e2_springdata_jpa/E2SpringdataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package de.freerider.e2_springdata_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E2SpringdataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
